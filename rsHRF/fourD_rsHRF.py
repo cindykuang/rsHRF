58a70bcf11e673311b30371fdb0236ce89d8cb5c
cindykuang <cindy.ck516@gmail.com>
2025-04-29 14:22:04 +0800

    took upsampling out of fourD_rsHRF will do in monitor

--- rsHRF/fourD_rsHRF.py
+++ rsHRF/fourD_rsHRF.py
@@ -133,10 +133,10 @@ def demo_rsHRF(input_file, mask_file, output_dir, para, p_jobs, file_type=".nii"
         os.makedirs(sub_save_dir, exist_ok=True)
         
     # right before saving: upsample hrfa for model #this whole section is added - changed
-    if 'upsampling_factor' in para or para['upsampling_factor'] != None:
-    	hrfa_t = hrfa.T
-    	upsample=lambda x : signal.resample_poly(x[::-1], para['upsampling_factor'], hrfa_t.shape[1])
-    	hrfa = np.apply_along_axis(upsample, 1, hrfa_t)
+    #if 'upsampling_factor' in para or para['upsampling_factor'] != None:
+    	#hrfa_t = hrfa.T
+    	#upsample=lambda x : signal.resample_poly(x[::-1], para['upsampling_factor'], hrfa_t.shape[1])
+    	#hrfa = np.apply_along_axis(upsample, 1, hrfa_t)
     	
     dic = {'para': para, 'hrfa': hrfa, 'event_bold': event_bold, 'PARA': PARA}
     ext = '_hrf.mat'
